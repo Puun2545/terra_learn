b7d2eb6258460d2edc1a9c12898ec4cfea825344
Puun2545 <puun.vach@gmail.com>
2024-03-21 15:55:04 +0700

    wk11 ebs attach

--- week-11/assignment_ebs/wk11_ebs.tf
+++ week-11/assignment_ebs/wk11_ebs.tf
@@ -3,9 +3,9 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  token      = ""
+  access_key = "ASIA6GBMCP37YS6H6YLU"
+  secret_key = "qFjVvlGFQJd/1NwCDHkGyPUCFFsHmoewU/kFGeHa"
+  token      = "FwoGZXIvYXdzECIaDMyKkrJIv353sgizLCLFAToa2U8a4qoHzZ8mD25o3xUkAQIpJpmj4qv9JZPPim8rVfH5Tj75G5UBQSN6kmA65cI0RbkSMIi88l9BSMQgXg5ytrSUUAbvdei/6j7HGnNkFGvFTN3+HH03dNqzb07L6H4dFTURAi2SAItKXyjys8G43aPNn6ZjY+MehTOIKJ96E2EvWyYzky401KRJCapNhlD+nKVl8+JQWGJS38w+RV0xSpe4gJ8DITLIVmuji0lsaSlfRSL0T+zniV3bj1duhk48K5HJKJrk768GMi2Yr48bs5tMa7n0LUlMxvAJ+ooQ2JTROxsSBH8Wpoc8Et9uoybH1KlfC56kPIQ="
   region     = "us-east-1"
 }
 
@@ -33,15 +33,12 @@ data "aws_ami" "aws-linux" {
   }
 }
 
-
 ##################################################################################
 # RESOURCES
 ##################################################################################
 
 #This uses the default VPC.  It WILL NOT delete it on destroy.
-resource "aws_default_vpc" "default" {
-
-}
+resource "aws_default_vpc" "default" {}
 
 resource "aws_security_group" "sg1" {
   name        = "npaWk11_demo"
@@ -67,7 +64,20 @@ resource "aws_instance" "testweb" {
   instance_type          = "t2.micro"
   key_name               = "vockey"
   vpc_security_group_ids = [aws_security_group.sg1.id]
+}
 
+# Define EBS volume
+resource "aws_ebs_volume" "testweb_volume" {
+  availability_zone = aws_instance.testweb.availability_zone
+  size              = 8
+  type              = "gp2"
+}
+
+# Attach EBS volume to EC2 instance
+resource "aws_volume_attachment" "testweb_attachment" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.testweb_volume.id
+  instance_id = aws_instance.testweb.id
 }
 
 ##################################################################################
